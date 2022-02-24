bfb1937fd945870a4616e46fd55091462e3ac67d
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-24 13:14:55 -0600

    set up index and user seeds

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config({path: '../.env'})
+const seedUser = require('./user-seeds')
+
+const sequelize = require('../config/connection')
+
+const statusUpdate = (message) => {
+  console.log('======================')
+  console.log(message)
+  console.log('======================')
+}
+
+const seedAll = async () => {
+
+  await sequelize.sync({force: true})
+  statusUpdate('DATABASE SYNCED');
+  try {
+    await seedUser();
+    statusUpdate('USERS SEEDED');
+
+    // await seedPost();
+    // statusUpdate('POSTS SEEDED');
+
+    // await seedComments();
+    // statusUpdate('COMMENTS SEEDED');
+  } catch (e) {
+    statusUpdate(e)
+  }
+  statusUpdate('EVERYTHING HAS BEEN SEEDED SUCCESSFULLY')
+  process.exit(0)
+}
+
+try {
+  (async () => {
+    await seedAll()
+  })()
+} catch (e) {
+  statusUpdate(e)
+}
