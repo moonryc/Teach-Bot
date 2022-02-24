c3042ad94e46b84c10cca544a419543d7f5fb875
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-24 13:19:42 -0600

    set up routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,80 @@
+const router = require('express').Router()
+const {User} = require('../../models/index')
+
+//LOGIN route
+router.post('/login', async (req, res) => {
+  try {
+    const document = await User.findOne({
+      where: {
+        username: req.body.username
+      },
+    })
+
+    if (!document) {
+      return res.status(400).json({message: "user does not exist"})
+    }
+    const isPasswordValid = await document.isPasswordValid(req.body.password)
+
+    if(!isPasswordValid){
+      return res.status(400).json({message: "incorrect password"})
+    }
+
+    req.session.save(()=>{
+      req.session.user_id = document.id
+      req.session.username = document.username
+      req.session.isLoggedIn = true
+
+      return res.json({
+        user: document,
+        message:"you are now logged in"
+      })
+    })
+
+  } catch (e) {
+    res.status(500).json({message: "Error", error: e})
+  }
+})
+
+//LOGOUT route
+router.post('/logout',(req,res)=>{
+
+  if(req.session.isLoggedIn){
+    req.session.destroy(()=>{
+      return res.status(204).end();
+    })
+  }else{
+    console.log("this ran")
+    return res.status(404).end();
+  }
+})
+
+//CREATE account route
+router.post('/',async (req,res) =>{
+
+  try{
+    const document = await User.create({
+      username:req.body.username,
+      email:req.body.email,
+      password:req.body.password
+    })
+
+    if(!document){
+      return res.status(500).json({message: "Error"})
+    }
+
+    return req.session.save(()=>{
+      req.session.user_id = document.id
+      req.session.username = document.username
+      req.session.isLoggedIn = true;
+
+      res.json(document)
+    })
+
+  }catch(e){
+    res.status(500).json({message:"error", error:e})
+  }
+
+})
+
+
+module.exports = router
---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router()
+const apiRoutes = require('./api')
+const homeRoutes = require('./home-routes')
+
+router.use('/',homeRoutes)
+router.use('/api',apiRoutes)
+
+//ERROR HANDLER
+router.use((req, res) => {
+  console.log("==========================================")
+  console.log("ERROR HANDLER")
+  console.log("==========================================")
+  res.status(404).end();
+});
+
+module.exports = router
