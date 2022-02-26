7ae2843c49b6aae5fd493bbd457e55d31084bcaa
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-25 19:46:36 -0600

    converted middleware to axios so that this is able to run on older versions of node

--- middleware/a21Handler.js
+++ middleware/a21Handler.js
@@ -1,8 +1,10 @@
+const aiQuestionFetcher = require('../utils/aiQuestionFetcher');
+
 const a21Handler = async (req, res, next) => {
   const { question } = req.body;
 
   let prompt =
-    'SalBot is your tutor. He has knowledge of javascript, html, css and can answer your questions in a clear and concise manner.' +
+    'TeachBot is your tutor. He has knowledge of javascript, html, css and can answer your questions in a clear and concise manner.' +
     '\nThe following is a conversation between you and SalBot. The conversation will follow the following format' +
     '\nSalBot: "Hello welcome to my tutoring session."' +
     '\nYou: "Thank you."' +
@@ -11,41 +13,8 @@ const a21Handler = async (req, res, next) => {
 
   //TODO GET MESSAGE HISTORY
   const lastFiveMessages = '';
-
-  const body = {
-    prompt: prompt + lastFiveMessages + question,
-    numResults: 1,
-    maxTokens: 100, //Max number of characters the AI can respond with
-    stopSequences: ['"'], //What is the stopping symbol that the AI will end with
-    topKReturn: 0, //DO NOT TOUCH
-    temperature: 0.7, //DO NO TOUCH
-  };
-
-  try {
-    const response = await fetch(
-      'https://api.ai21.com/studio/v1/j1-large/complete',
-      {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.AI21_API_KEY}`,
-        },
-        body: JSON.stringify(body),
-      }
-    );
-
-    if (!response.ok) {
-      throw await response.json();
-    }
-
-    const data = await response.json();
-
-    console.log(data);
-    req.body.answer = data.completions[0].data.text.slice(0);
-    next();
-  } catch (e) {
-    console.log(e);
-  }
+  const fullQuestion = prompt + lastFiveMessages + question;
+  req.body.answer = await aiQuestionFetcher(fullQuestion);
+  next();
 };
-
 module.exports = a21Handler;
