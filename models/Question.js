3136757725ae0bfd327d37f0c32c28e2e5c4b4d1
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-25 18:12:52 -0600

    new models Message.js and Question.js, subsequently updated the model index and created the associations

---
+++ models/Question.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Question extends Model {}
+
+Question.init(
+  {
+    id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    topic: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'question',
+  }
+);
+
+module.exports = Question;
