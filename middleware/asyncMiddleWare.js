d233662864f41f6b8f85c95853f7c62d21348d6a
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-26 19:51:53 -0600

    updated and removed uneccessary files

--- middleware/asyncMiddleWare.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncMiddleWare = (asyncFunction) => (req, res, next) => {
-    Promise.resolve(asyncFunction(req, res, next)).catch((error) => console.log(error));
-};
-exports.default = asyncMiddleWare;
