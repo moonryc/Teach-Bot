54a2c3b7ae7b3ec9aca604d2d7ee7eda95d22e2c
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-28 19:27:20 -0600

    todos

--- controllers/home-routes.ts
+++ controllers/home-routes.ts
@@ -5,16 +5,21 @@ const homeRoutes = express.Router();
 homeRoutes.get('/', async (req, res) => {
   console.log(req.session);
   console.log(res);
+  //TODO check if the user is already logged in, if the user is route them to the AIChatbot
 });
 
 //LOGIN PAGE
 homeRoutes.get('/login', (req, res) => {
+  //TODO check if user is already logged in, if logged in routee them to the AIChatbot
   res.render('login');
 });
 
 //SIGNUP PAGE
 homeRoutes.get('/signup', (req, res) => {
   res.render('signup');
+  //TODO check if user is already logged in, if logged in routee them to the AIChatbot
 });
+
+//TODO add a route that renders AI chatbot and passes the topics that are related to them as well as the messages associated with that topic
 
 export default homeRoutes;
