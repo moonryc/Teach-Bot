65639a6ffd62ac2f3fa487b5d9235656f0cbc97d
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-06 20:32:23 -0600

    my frontend

--- public/javascript/message.js
+++ public/javascript/message.js
@@ -1,9 +1,39 @@
-var messages = document.getElementById("messages");
-var chatbox =document.getElementById("chatbox");
-var button = document.getElementById("button");
+const submitQuestion = async (e) => {
+  e.preventDefault();
 
-button.addEventListener("click", function() {
-    var newMessage = document.createElement("li");
-    newMessage.innerHTML = chatbox.value;
-    messages.appendChild(newMessage);
-});
+  const topic =
+    window.location.pathname.split('/')[
+      window.location.pathname.split('/').length - 1
+    ];
+  const question = document.querySelector('.message-input').value;
+
+  if (!question) {
+    return alert('Please submit a non-empty message');
+  }
+
+  const response = await fetch(`/api/question/${topic}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ question }),
+  });
+
+  const body = await response.json();
+
+  if (!response.ok) {
+    alert('response is not okay');
+  }
+
+  const messagesEl = document.getElementById('messages');
+
+  messagesEl.append(`<div>
+  
+<!--  human-->
+  <div class="human-message">${question}</div>
+<!--  ai-->
+  <div class="ai-message">${body.message}</div>
+</div>`);
+};
+
+document
+  .querySelector('#questionForm')
+  .addEventListener('submit', (e) => submitQuestion(e));
