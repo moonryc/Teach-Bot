f0e851825d35c3fc44cfcd9f45a7689f164f2b5c
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-07 00:42:27 -0600

    added the ability to signup, and made the AI smarter

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -9,9 +9,9 @@ async function loginFormHandler(event) {
       method: 'post',
       body: JSON.stringify({
         username: email,
-        password: password,
+        password: password
       }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
@@ -22,6 +22,41 @@ async function loginFormHandler(event) {
   }
 }
 
+const signupHandler = async (e) => {
+  e.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (!email || !password || !username) {
+    return alert('something was left blank when attempting to sign up');
+  }
+  const response = await fetch('/api/users/', {
+    method: 'POST',
+    body: JSON.stringify({
+      username, email, password
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  const data = await response.json();
+
+  if(!response.ok){
+    console.log(data);
+    return alert(data.message);
+  }
+
+  window.location.replace('/chatbot/0');
+  console.log(data);
+
+};
+
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', (e) => signupHandler(e));
